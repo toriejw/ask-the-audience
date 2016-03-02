64283afb9ae38afe84668c5aeaacfd61edfa503b
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-03-02 09:53:10 -0700

    Allow users to cast votes

---
+++ public/client.js
@@ -0,0 +1,21 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+var votes = {};
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -11,9 +13,34 @@ app.get('/', function(req, res){
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app)
-                  .listen(port, function () {
-                    console.log('Listening on port ' + port);
-                  });
+const server = http.createServer(app);
+server.listen(port, function () {
+              console.log('Listening on port ' + port);
+            });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount); // sends to all clients
+
+  socket.emit('statusMessage', 'You have connected.'); // sends to a single client
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+    }
+  });
+});
 
 module.exports = server;
